2320eb15e4b6226a5601aa0e0b63c7b759c71cfb
Austin Norquist <austin.norquist@liatrio.com>
2025-11-16 14:01:34 -0800

    fix: new feature

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Austins node app!\n');
 });
 
 server.listen(PORT, HOST, () => {
