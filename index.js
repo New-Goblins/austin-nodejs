d6d49b942e7fb48ca1f745a6005ba348318705f7
Austin Norquist <austin.norquist@liatrio.com>
2025-11-17 10:06:28 -0800

    fix: monday

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('today is monday\n');
+  res.end('today is monday!!!!\n');
 });
 
 server.listen(PORT, HOST, () => {
